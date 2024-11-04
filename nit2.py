40d73be7959cbffc4356b1e7648a8c8dd5789da4
VamshiTejaDev <vamshicat2023@gmail.com>
2024-11-04 15:53:45 +0530

    my 2nd python code

---
+++ nit2.py
@@ -0,0 +1,18 @@
+a=int(input("A"))
+b=int(input("B"))
+c=int(input("C"))
+if a>b:
+    if c>a:
+        print(c ," C is big" )
+    else:
+        print(a , ' A is big')
+elif c>b :
+    if c>a:
+        print(c ," C is big" )
+    else:
+        print(a , ' A is big')
+else:
+    print(b , ' B is big')
+    
+
+    
